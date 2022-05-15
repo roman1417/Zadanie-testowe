6e017be3030cd09c8f203e80911ad8ba75deabd4
Roman Popielewski <roman.popielewski@op.pl>
2022-05-15 20:42:02 +0200

    Add styles to InfoBlock component

--- src/components/info/InfoBlock.vue
+++ src/components/info/InfoBlock.vue
@@ -17,3 +17,67 @@ export default {
   name: 'InfoBlock',
 };
 </script>
+
+<style lang="scss" scoped>
+.info-block {
+  margin-bottom: 120px;
+  text-align: center;
+
+  @include mq(xlarge) {
+    margin-bottom: 100px;
+  }
+
+  @include mq(large) {
+    margin-bottom: 85px;
+  }
+
+  @include mq(medium) {
+    margin-bottom: 75px;
+  }
+
+  @include mq(small) {
+    margin-bottom: 60px;
+  }
+
+  &__title {
+    @include column;
+  }
+
+  &__head {
+    margin-bottom: 0.1em;
+    font-size: 2.8rem;
+    line-height: 1.5;
+
+    @include mq(xlarge) {
+      font-size: 2.5rem;
+    }
+
+    @include mq(large) {
+      font-size: 2.2rem;
+    }
+
+    @include mq(medium) {
+      font-size: 2rem;
+    }
+
+    @include mq(small) {
+      font-size: 1.8rem;
+    }
+  }
+
+  &__underline {
+    position: relative;
+    display: inline-block;
+
+    &::after {
+      content: '';
+      position: absolute;
+      left: -0.13em;
+      width: calc(100% + 0.26em);
+      bottom: 0.11em;
+      height: 0.11em;
+      background-color: $orange;
+    }
+  }
+}
+</style>
