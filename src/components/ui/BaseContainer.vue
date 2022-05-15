3e2bdb8d33984f9e0af92a66690475640822fd9d
Roman Popielewski <roman.popielewski@op.pl>
2022-05-15 08:49:21 +0200

    Add styles to BaseContainer component

--- src/components/ui/BaseContainer.vue
+++ src/components/ui/BaseContainer.vue
@@ -9,3 +9,22 @@ export default {
   name: 'BaseContainer',
 };
 </script>
+
+<style lang="scss" scoped>
+.container {
+  width: 1180px;
+  margin: 0 auto;
+
+  @include mq(xlarge) {
+    width: 980px;
+  }
+
+  @include mq(large) {
+    width: 720px;
+  }
+
+  @include mq(medium) {
+    width: 90%;
+  }
+}
+</style>
