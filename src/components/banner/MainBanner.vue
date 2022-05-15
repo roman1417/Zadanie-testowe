266194398cb8609da474b11462eedef9fa131b13
Roman Popielewski <roman.popielewski@op.pl>
2022-05-15 09:45:54 +0200

    Add MainBanner Component

---
+++ src/components/banner/MainBanner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="banner">
+    <base-container>
+      <div class="banner__claim">
+        <div class="banner__breadcrumbs"></div>
+        <h1 class="banner__title"></h1>
+      </div>
+    </base-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainBanner',
+};
+</script>
