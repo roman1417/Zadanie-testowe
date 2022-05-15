e80df79bbcf1f25fed857868e6cf2199282de285
Roman Popielewski <roman.popielewski@op.pl>
2022-05-15 08:38:10 +0200

    Add basic global styles

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,17 @@ export default {
   name: 'App',
 };
 </script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;500;600;700&display=swap');
+
+html {
+  font-size: 10px;
+}
+
+body {
+  font-family: $maven;
+  color: $dark;
+  background-color: $background;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  css: {
+    sourceMap: true,
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/global-styles.scss";`,
+      },
+    },
+  },
+});
