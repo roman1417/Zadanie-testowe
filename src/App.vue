34a1fc17647cd144d471413384ce8952a7c8ec91
Roman Popielewski <roman.popielewski@op.pl>
2022-05-15 16:23:22 +0200

    Add profits data

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,31 @@ export default {
         path: ['Strona główna'],
         currentPage: 'Franczyza',
       },
+      profits: {
+        item1: {
+          title: 'Zostaniesz otoczony opieką dedykowanego partnera',
+          text: 'W Odido nie zostaniesz sam! Zapewnimy Ci opiekę doradcy, który zawsze chętnie pomoże w doborze asortymentu, przedstawi najkorzystniejsze oferty, czy przypomni o wzięciu udziału w konkursie.',
+          image: './assets/images/profit1.jpg',
+          imageOnLeft: true,
+          alt: 'kobieta i mężczyzna rozmawiają w sklepie',
+          icon: './assets/images/icon1.svg',
+        },
+        item2: {
+          title: 'To Ty decydujesz, a my Cię wspieramy',
+          text: 'Wiemy, że cenisz niezależność. Dlatego my skupiamy się na zapewnieniu Ci najbardziej korzystnych warunków, abyś Ty mógł zająć się prowadzeniem biznesu.',
+          image: './assets/images/profit2.jpg',
+          imageOnLeft: false,
+          alt: 'mężczyzna z tabletem przy półce sklepowej',
+          icon: './assets/images/icon2.svg',
+        },
+        item3: {
+          title: 'MAKRO dla Franczyzobiorców Odido',
+          text: 'Jako franczyzobiorca Odido możesz dostać nawet 7% rabatu na asortyment dostępny w MAKRO. Wszystko, czego potrzebują Twoi klienci możesz kupić na przestrzennej, dobrze wyposażonej hali lub zamówić przez naszą platformę do zamówień, a my dostarczymy zamówienie do Twojego sklepu. Do tego mamy szereg programów i aktywacji takich jak program Zielone Etykiety, Retro Bony, czy Program Koszyki, które naprawdę się opłacają. Mamy też atrakcyjne konkursy dedykowane tylko dla Franczyzobiorców Odido. ',
+          image: './assets/images/profit3.jpg',
+          imageOnLeft: true,
+          alt: 'hala sklepowa w "Makro"',
+        },
+      },
     };
   },
   components: {
